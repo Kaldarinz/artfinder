9449460db6d6ab2ddad37227e1a8fc4ffc8a3a85
Anton Popov <a.popov.fizteh@gmail.com>
2025-04-02 12:40:53 +0300

    regex filter for titles

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-from ast import literal_eval
+import re
 
-print(literal_eval("[{'start': {'date-parts': [[2021, 10, 1]], 'date-time': '2021-10-01T00:00:00Z', 'timestamp': 1633046400000}, 'content-version': 'vor', 'delay-in-days': 0, 'URL': 'http://creativecommons.org/licenses/by/3.0/'}, {'start': {'date-parts': [[2021, 10, 1]], 'date-time': '2021-10-01T00:00:00Z', 'timestamp': 1633046400000}, 'content-version': 'tdm', 'delay-in-days': 0, 'URL': 'https://iopscience.iop.org/info/page/text-and-data-mining'}]"))
-print(literal_eval('None'))
+# Example list of titles
+title = 'advanced nanoparticle generation and excitation by lasers in liquids'
+#patterns
+parts = [
+    r'(?=.*\blaser\w*\b)(?=.*\bgener\w*|.*\bsynth\w*)(?=.*\bnano\w*\b|.*\bcolloid\w*)',
+    r'(?=.*\bnanop\w*)(?=.*(ablat\w*|fragment\w*))'
+]
+pattern = r'|'.join(parts)
+
+# Filter titles that match the pattern
+print(re.search(pattern, title, re.IGNORECASE))
