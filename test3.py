3da93713c40f1b8e8368c47ac60519df431e9ea2
Anton Popov <a.popov.fizteh@gmail.com>
2025-04-16 19:08:20 +0300

    tests

--- test3.py
+++ test3.py
@@ -1,11 +1,12 @@
 import logging
 import artfinder
 
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 from artfinder.article import load_csv
 
 cr = artfinder.Crossref()
 af = artfinder.ArtFinder()
 
-af.search('laser synthesis of colloids', pub_since='2010')
+df = af.search('TiN nanoparticles', author='kabashin', pub_since='2018', max_results=None)
+new_obj = af.get_refs(df[:2])
